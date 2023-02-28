cad3a9321d158fb97f984493575b99c898ff194c
owaisrazaeng1 <owais.raza.eng458@gmail.com>
2023-02-28 14:12:59 +0500

    set up routes and gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "pages#home"
+
+  resources :search_queries
+
+  namespace :api do
+    namespace :v1 do
+      devise_for :users
+      resources :search_queries, only: [:show, :create] do
+        resources :keyword_search_results, only: [:show]
+      end
+    end
+  end
+
+  get "search_queries/:search_query_id/keyword_search_results/:id", to: "api/v1/keyword_search_results#show", as: "keyword_search_result"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
